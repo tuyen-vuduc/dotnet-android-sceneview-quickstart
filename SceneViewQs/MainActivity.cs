81f3e30acb6087980d74f3a61014ec6b6c4e5cba
Tuyen Vu <hi@tuyen-vuduc.tech>
2024-02-14 07:42:04 +0700

    - [WIP] ModelViewer sample

--- SceneViewQs/MainActivity.cs
+++ SceneViewQs/MainActivity.cs
@@ -1,14 +1,104 @@
+using Android.Views;
+using AndroidX.AppCompat.App;
+using AndroidX.Core.Graphics;
+using AndroidX.Core.View;
+
 namespace SceneViewQs
 {
-    [Activity(Label = "@string/app_name", MainLauncher = true)]
-    public class MainActivity : Activity
+    [Activity(Label = "@string/app_name", MainLauncher = true, Theme = "@style/Theme.SceneViewSample")]
+    public class MainActivity : AppCompatActivity
     {
         protected override void OnCreate(Bundle? savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.activity_main);
+            SetContentView(Resource.Layout.activity);
+
+            setFullScreen(
+                FindViewById(Resource.Id.rootView),
+                fullScreen: true,
+                hideSystemBars: false,
+                fitsSystemWindows: false
+            );
+
+            var toolbar = FindViewById<AndroidX.AppCompat.Widget.Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+            toolbar.doOnApplyWindowInsets((systemBarsInsets) =>
+            {
+                (toolbar.LayoutParameters as ViewGroup.MarginLayoutParams).TopMargin = systemBarsInsets.Top;
+                toolbar.Title = string.Empty;
+            });
+
+            var fragmentTransaction = SupportFragmentManager.BeginTransaction();
+            fragmentTransaction.Add(Resource.Id.containerFragment, new MainFragment(), new Bundle());
+            fragmentTransaction.Commit();
+        }
+
+        void setFullScreen(
+            View rootView,
+            bool fullScreen = true,
+            bool hideSystemBars = true,
+            bool fitsSystemWindows = true
+        )
+        {
+            rootView.ViewTreeObserver.WindowFocusChange += (sender, e) =>
+            {
+                if (e.HasFocus)
+                {
+                    WindowCompat.SetDecorFitsSystemWindows(Window, fitsSystemWindows);
+                    var wicc = new WindowInsetsControllerCompat(Window, rootView);
+
+                    if (hideSystemBars)
+                    {
+                        if (fullScreen)
+                        {
+                            wicc.Hide(
+                                WindowInsetsCompat.Type.StatusBars() |
+                                WindowInsetsCompat.Type.NavigationBars()
+                            );
+                        }
+                        else
+                        {
+                            wicc.Show(
+                                WindowInsetsCompat.Type.StatusBars() |
+                                WindowInsetsCompat.Type.NavigationBars()
+                            );
+                        }
+
+                        wicc.SystemBarsBehavior = WindowInsetsControllerCompat.BehaviorShowTransientBarsBySwipe;
+                    }
+                }
+            };
+        }
+    }
+
+    public static class ViewExtensions
+    {
+        public static void doOnApplyWindowInsets(this View view, Action<Insets> action)
+        {
+            view.ViewAttachedToWindow += (s, e) =>
+            {
+                ViewCompat.SetOnApplyWindowInsetsListener(view, new XOnApplyWindowInsetsListener(action));
+            };
+        }
+
+        sealed class XOnApplyWindowInsetsListener : Java.Lang.Object, IOnApplyWindowInsetsListener
+        {
+            private Action<Insets> action;
+
+            public XOnApplyWindowInsetsListener(Action<Insets> action)
+            {
+                this.action = action;
+            }
+
+            public WindowInsetsCompat OnApplyWindowInsets(View v, WindowInsetsCompat insets)
+            {
+                action(insets.GetInsets(WindowInsetsCompat.Type.SystemBars()));
+
+                return WindowInsetsCompat.Consumed;
+            }
         }
     }
 }
---
+++ SceneViewQs/MainFragment.cs
@@ -0,0 +1,75 @@
+﻿using Android.Views;
+using IO.Github.Sceneview;
+using IO.Github.Sceneview.Node;
+using Kotlin.Jvm.Functions;
+using static Android.Icu.Text.Transliterator;
+using static Com.Google.Android.Filament.Utils.IBLPrefilterContext;
+
+namespace SceneViewQs;
+
+public partial class MainFragment : AndroidX.Fragment.App.Fragment
+{
+    SceneView sceneView;
+    View loadingView;
+
+    public override View? OnCreateView(LayoutInflater inflater, ViewGroup? container, Bundle? savedInstanceState)
+    {
+        return inflater.Inflate(Resource.Layout.fragment_main, container, false);
+    }
+
+    public override void OnViewCreated(View view, Bundle? savedInstanceState)
+    {
+        base.OnViewCreated(view, savedInstanceState);
+
+        sceneView = view.FindViewById<SceneView>(Resource.Id.sceneView);
+        sceneView.Lifecycle = Lifecycle;
+
+        loadingView = view.FindViewById(Resource.Id.loadingView);
+
+        var lifeCycleScope = AndroidX.Lifecycle.LifecycleOwnerKt.GetLifecycleScope(this);
+
+        lifeCycleScope.LaunchWhenCreated(this);
+    }
+}
+
+partial class MainFragment : IFunction2
+{
+    public Java.Lang.Object? Invoke(Java.Lang.Object? p0, Java.Lang.Object? p1)
+    {
+        var hdrFile = "environments/studio_small_09_2k.hdr";
+
+        var light = sceneView.LoadHdrIndirectLight(hdrFile, specularFilter: true);
+        light.Intensity = (30_000f);
+
+        var skybox = sceneView.LoadHdrSkybox(hdrFile);
+        skybox.Intensity(50_000f);
+
+        var model = sceneView.ModelLoader.LoadModel("models/MaterialSuite.glb", null, null);
+        var modelNode = new ModelNode(sceneView, model); 
+        
+        {
+            transform(
+                position = Position(z = -4.0f),
+                rotation = Rotation(x = 15.0f)
+            )
+                scaleToUnitsCube(2.0f)
+                // TODO: Fix centerOrigin
+                //  centerOrigin(Position(x=-1.0f, y=-1.0f))
+                playAnimation()
+            }
+        sceneView.addChildNode(modelNode)
+
+            val viewNode = ViewNode(
+                sceneView = sceneView,
+                viewResourceId = R.layout.view_node_layout
+            ).apply {
+            transform(
+                position = Position(z = -4f),
+                rotation = Rotation()
+            )
+            }
+        sceneView.addChildNode(viewNode)
+
+            loadingView.isGone = true
+    }
+}
